ce2003353eb624b8468a49fa64a44cb05807aab1
YagmurCelikkaya <ry.celikkaya@gmail.com>
2024-10-26 15:35:39 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -7,8 +7,12 @@ int main(void) {
     scanf("%d", &guess);
     if(guess == rightAnswer) {
         printf("It is correct.\n");
-    } else {
+    }else if(guess<=0 || guess>=5){
+        printf("Your guess is out of boundary.\n");
+    }else {
         printf("Your guess is wrong.");
 
     }
 }
+
+
