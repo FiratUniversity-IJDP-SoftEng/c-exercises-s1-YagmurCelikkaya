0e4017d6f460b363418a8f45d0c5a1b670f2b5fb
YagmurCelikkaya <ry.celikkaya@gmail.com>
2024-10-26 14:10:06 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int a = get_int("Please enter your age: ");
+    int counter = a;
+    while (counter > 0)
+    {
+        printf("Happy Birthday!\n");
+        counter = counter -1;
+    }
+}
 
