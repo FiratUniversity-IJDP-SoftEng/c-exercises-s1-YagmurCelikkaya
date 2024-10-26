afacb5754df01b32cbea5a0a8cce90848bd5a592
YagmurCelikkaya <ry.celikkaya@gmail.com>
2024-10-26 14:07:33 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int guess;
+    int rightAnswer = 3;
 
+    printf("Guess a number between 1 and 5 : ");
+    scanf("%d", &guess);
+    if(guess == rightAnswer) {
+        printf("It is correct.\n");
+    } else {
+        printf("Your guess is wrong.");
+
+    }
+}
